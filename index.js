2cc1fc9e2322c4e0ae4f0e4d502f85a16b2b0506
StaeyKay <ekwashie001@st.ug.edu.gh>
2024-06-27 15:51:59 +0000

    Express and MongoDB

--- index.js
+++ index.js
@@ -3,26 +3,13 @@ import mongoose from "mongoose";
 import recipeRouter from "./routes/recipe.js";
 
 // Connect to database
-await mongoose.connect(process.env.mongo_url);
+await mongoose.connect(process.env.mongo_url);1
 
 // Create Express App
 const app = express();
 
 // Apply middlewares
 app.use(express.json());
-
-// Define routes
-app.get('/', (req, res) => {
-    res.json('Welcome home')
-});
-
-app.post('/login', (req, res) => {
-    res.json('Login successful')
-});
-
-app.patch('/define', (req, res) => {
-    res.json('You just defined a new endpoint')
-})
 
 // Use routes. Other routes defined in other files can be used here
 app.use(recipeRouter);
